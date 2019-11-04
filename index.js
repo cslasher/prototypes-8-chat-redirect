be081515c4ac89a2781307914b9f8d5e617642c4
Edwin Liu <edwin@no8.io>
2019-11-04 12:19:29 +0800

    Init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/member_login', (req, res) => {
+  let response = {
+    user: 'user',
+    password: 'password',
+    client_id: req.body.client_id
+  };
+
+  res.json(response);
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at port: ${PORT}`);
+});
